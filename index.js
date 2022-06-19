90314a5e9adbd0b2e09381e152486d4ba43ed35f
ARIFULHAQUE1 <arifulhaquearif2326@gmail.com>
2022-06-20 04:12:21 +0600

    deploy heroku

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const app = express();
 require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
@@ -9,56 +10,89 @@ app.use(express.json());
 const port = process.env.PORT || 5000;
 // connect to  database
 
-
+  // verify jwt 
+  function verifyJwt (req,res,next){
+      const authHeaders= req.headers.authorization;
+   
+      if(!authorization){
+            // return res.status(401).send({messge:'unAuthorazie Access'})
+      }
+      next();
+      console.log('inside jwt verify',authHeaders);
+            }
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kfn4m.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-async function run(){
+async function run() {
       await client.connect();
       const serviceCollection = client.db('geniusCar').collection('service');
-try{
-      // find users from data base
-app.get('/service',async(req,res)=>{
-const query = {};
-const cursor = serviceCollection.find(query);
-const services = await cursor.toArray();
-res.send(services);
+      const orderCollection = client.db('geniousCar').collection('order');
+      try {
 
-})
-// find user from data base
-app.get('/service/:id',async(req,res)=>{
-      const id = req.params.id;
-      const query = {_id: ObjectId(id)};
-      const service = await serviceCollection.findOne(query);
-      res.send(service);
-})
-// post data to database
-app.post('/service',async(req,res)=>{
-const newService = req.body;
-const result = await serviceCollection.insertOne(newService);
-res.send(result);
-})
+            // JWT TOKENT API
+            app.post('/login', async (req, res) => {
+                  const user = req.body;
+                  const accessToken = jwt.sign(user, process.env.SECRET_TOKEN, { expiresIn: '1d' });
+                  res.send({ accessToken });
+            })
 
-// delete data from database
- app.delete('/service/:id',async(req,res)=>{
-       const id = req.params.id;
-       const query = {_id:ObjectId(id)};
-       const result =await serviceCollection.deleteOne(query);
-       res.send(result); 
-    
+            // find users from data base
+            app.get('/service', async (req, res) => {
+                  const query = {};
+                  const cursor = serviceCollection.find(query);
+                  const services = await cursor.toArray();
+                  res.send(services);
 
- })
+            })
+            // find user from data base
+            app.get('/service/:id', async (req, res) => {
+                  const id = req.params.id;
+                  const query = { _id: ObjectId(id) };
+                  const service = await serviceCollection.findOne(query);
+                  res.send(service);
+            })
+            // post data to database
+            app.post('/service', async (req, res) => {
+                  const newService = req.body;
+                  const result = await serviceCollection.insertOne(newService);
+                  res.send(result);
+            })
 
-}
-finally{
+            // delete data from database
+            app.delete('/service/:id', async (req, res) => {
+                  const id = req.params.id;
+                  const query = { _id: ObjectId(id) };
+                  const result = await serviceCollection.deleteOne(query);
+                  res.send(result);
+            })
 
-}
+            // order collection api 
+            app.get('/order',verifyJwt, async (req, res) => {
+                 
+                  const email = req.query.email;
+                  console.log(email);
+                  const query = { email: email };
+                  const cursor = orderCollection.find(query);
+                  const orders = await cursor.toArray();
+                  res.send(orders);
+            })
+            app.post('/order', async (req, res) => {
+                  const order = req.body;
+                  const result = await orderCollection.insertOne(order);
+                  res.send(result);
+            })
+
+          
+      }
+      finally {
+
+      }
 }
 run().catch(console.dir);
 
 
-app.get('/',(req,res)=>{
-      res.send('hello node js world')
+app.get('/', (req, res) => {
+      res.send('genious cars node js world')
 })
- app.listen(port,()=>{
-       console.log('listening port ',port);
- })
+app.listen(port, () => {
+      console.log('listening port ', port);
+})
